69732dbedd54ee6d388cd11a1b6a58118e548653
IYADUKUNZE Emile <80049630+emilenfc@users.noreply.github.com>
2023-01-23 08:33:05 +0200

    update

--- app.js
+++ app.js
@@ -75,10 +75,10 @@ app.use(cookieParser())
 app.use(routes)
 
 mongoose
-	//.connect("mongodb+srv://Emile:KwaNgujezcHJkvEf@cluster0.ancg36w.mongodb.net/MyBrandfDB?retryWrites=true&w=majority", { useNewUrlParser: true })
-	.connect("mongodb://localhost:27017/MyBrandfDB", { useNewUrlParser: true })
+	.connect("mongodb+srv://Emile:KwaNgujezcHJkvEf@cluster0.ancg36w.mongodb.net/MyBrandfDB?retryWrites=true&w=majority", { useNewUrlParser: true })
+	//.connect("mongodb://localhost:27017/MyBrandfDB", { useNewUrlParser: true })
 	.then(async () => {
-		await app.listen(4000, () => { //here changed by adding process.env.PORT
+		await  app.listen(4000, () => { //here changed by adding process.env.PORT
 			console.log("Your Server has started on port 4000!")
 			swaggerDocs(app, 4000)
 		})
